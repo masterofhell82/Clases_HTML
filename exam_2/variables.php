20db063b03b1bd23542c962b2033bb9da3713aea
Maria Garcia <marialejandra.g98@gmail.com>
2019-04-16 01:55:34 -0400

    evaluacion de php

---
+++ exam_2/variables.php
@@ -0,0 +1,16 @@
+<?php
+           
+$edad= 51;                          //variable edad es de tipo numérico
+$nombre= 'Santiago ';               //variable nombre es de tipo string
+$apellido= 'Gutierrez';             //variable apellido es de tipo string
+$ocupacion= 'contador';             //variable ocupacion es de tipo string
+$peso= 100;                         //variable peso es de tipo numérico. El peso estará expresado en kilogramos
+$altura= 175;                       //variable altura es de tipo numérico. La altura estará expresada en centímetros
+$sexo= 'hombre';                    //variable sexo es de tipo string
+$alergias= false;                   //variable alergias es de tipo booleano (verdadero o falso)
+$diabetes= true;                    //variable diabetes es de tipo booleano (verdadero o falso)
+$tension= false;                    //variable tension es de tipo booleano (verdadero o falso)
+$colon= false;                      //variable colon es de tipo booleano (verdadero o falso)
+ 
+echo 'Nombre: ' . $nombre . '<br> Apellido: ' . $apellido  . '<br> Edad: ' . $edad  . '<br> Ocupación: ' . $ocupacion . '<br> Sexo: ' . $sexo . '<br> Peso: ' . $peso . 'kg' . '<br> Altura: ' . $altura . 'cm';
+ ?>
